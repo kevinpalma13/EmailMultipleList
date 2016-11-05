e9fcd227fa7ed14a436c51f08743943d332ae1d6
Kevin Palma Lopez <kevin.palma13@gmail.com>
2016-11-05 13:10:32 +0100

    Add files via upload

---
+++ emailList.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	$("#inputEmail").keyup(function(event) {
+  		var inputText = $("#inputEmail").val();
+  		var lastChar = inputText.substr(inputText.length - 1);
+  		if(event.keyCode == 186 && lastChar == ';'){
+	  		if(validateEmail(inputText)){
+	  			var emailtext = inputText.split(";", 1);
+	  			$("#listEmails").append("<div id='"+emailtext+"' class='emailtext'>"+emailtext+"<div id='emailtext-del'>X</div></div>");
+	  			$("#inputEmail").val("");
+
+				$("#listEmails #emailtext-del").click(function(){
+					$(this).parent().remove();
+				});
+	  			
+				$("#inputEmail").attr("style","color:black");
+	  		}
+	  		else{
+	  			$("#inputEmail").val(inputText.split(";", 1));
+	  			$("#inputEmail").attr("style","color:red");
+	  		}
+	  	}
+	});
+
+})
+
+function validateEmail(inputEmail) {
+	var res = inputEmail.split(";", 1);
+  	var filter = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return filter.test(res);
+}
+
+
+
